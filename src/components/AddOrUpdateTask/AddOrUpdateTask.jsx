cfb1f40b4b3dc80809dd0def3a5f7f2fa4bcfe6c
urooj khan <khanurooj5299@gmail.com>
2024-04-01 23:56:47 +0530

    Modified add-update comp

--- src/components/AddOrUpdateTask/AddOrUpdateTask.jsx
+++ src/components/AddOrUpdateTask/AddOrUpdateTask.jsx
@@ -1,41 +1,52 @@
-import { addTask, getTaskList, updateTask } from '../../http';
-import { getDateString } from '../../utilities';
-import './AddOrUpdateTask.css';
+import { addTask, updateTask } from "../../http";
+import { getDateString } from "../../utilities";
+import "./AddOrUpdateTask.css";
 
-export default function AddOrUpdateTask({updateId, setTaskList}) {
-  const isUpdate = updateId!=null;
+export default function AddOrUpdateTask({ updateId, setUpdateId, setTaskList }) {
+  const isUpdate = updateId != null;
 
   async function handleClick() {
-    const title = document.getElementById('task-title').value;
-    const deadline = document.getElementById('task-deadline').value;
-    let res;
-    if(isUpdate) {
-      res = await updateTask({title, deadline, updateId});
-    } else {
-      res = await addTask({title, deadline});
-      console.log(res)
-      //if task was succesfully added the api returns the newly created task
-      if(res && res.status==200) {
-        setTaskList((prevTaskList)=>{
+    try {
+      const title = document.getElementById("task-title").value;
+      const deadline = document.getElementById("task-deadline").value;
+      let res;
+      if (isUpdate) {
+        res = await updateTask({ title, deadline, updateId });
+        //if it was update operation reset the UI to add
+        setUpdateId(null);
+      } else {
+        res = await addTask({ title, deadline });
+        //if task was succesfully added the api returns the newly created task
+        //change tasklist state to reflect newly added task
+        setTaskList((prevTaskList) => {
           return [res, ...prevTaskList];
         });
       }
-    }
-    if(res.status==200) {
-      getTaskList(setTaskList);
+      //finally reset the form
+      document.getElementById('add-update-form').reset();
+    } catch (err) {
+      //handle error
     }
   }
   return (
     <>
+    <form id="add-update-form">
       <div className="mb-3">
         <label className="form-label">Task Title</label>
-        <input type="text" className="form-control" id="task-title"/>
+        <input type="text" className="form-control" id="task-title" />
       </div>
       <div className="mb-3">
         <label className="form-label">Task Deadline</label>
-        <input type="date" className="form-control" min={getDateString(new Date())} id="task-deadline"/>
-      </div>
-      <button className="btn btn-primary" onClick={handleClick} type='button'>{isUpdate?"Update":"Add"}</button>
+        <input
+          type="date"
+          className="form-control"
+          min={getDateString(new Date())}
+          id="task-deadline"
+        />
+      </div></form>
+      <button className="btn btn-primary" onClick={handleClick} type="button">
+        {isUpdate ? "Update" : "Add"}
+      </button>
     </>
   );
 }
--- src/http.js
+++ src/http.js
@@ -1,51 +1,39 @@
 const apiUrl = "http://localhost:3000";
 
 export async function getTaskList(setTaskList) {
-  try {
-    const res = await fetch(`${apiUrl}/task/tasks`);
-    if (res.ok) {
-      const taskList = await res.json();
-      setTaskList(taskList);
-    } else {
-      throw new Error(res.statusText);
-    }
-  } catch (err) {
-    throw new Error(err);
+  const res = await fetch(`${apiUrl}/task/tasks`);
+  if (res.ok) {
+    const taskList = await res.json();
+    setTaskList(taskList);
+  } else {
+    throw new Error(res.statusText);
   }
 }
 
 export async function addTask(task) {
-  try {
-    const res = await fetch(`${apiUrl}/task/add-task`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(task),
-    });
-    if (res.ok) {
-      return await res.json();
-    }
-    throw new Error(res.statusText);
-  } catch (err) {
-    throw new Error(err);
+  const res = await fetch(`${apiUrl}/task/add-task`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(task),
+  });
+  if (res.ok) {
+    return await res.json();
   }
+  throw new Error(res.statusText);
 }
 
 export async function updateTask(task) {
-  try {
-    const res = await fetch(`${apiUrl}/task/update-task/${task.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(task),
-    });
-    if (res.ok) {
-      return await res.json();
-    }
-    throw new Error(res.statusText);
-  } catch (err) {
-    throw new Error(err);
+  const res = await fetch(`${apiUrl}/task/update-task/${task.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(task),
+  });
+  if (res.ok) {
+    return await res.json();
   }
+  throw new Error(res.statusText);
 }
