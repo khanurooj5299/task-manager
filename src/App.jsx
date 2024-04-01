caaeef653a3a2bc7287e5961d1ebf0aa529fa3f5
urooj khan <khanurooj5299@gmail.com>
2024-04-01 17:35:11 +0530

    Added Header componenet

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,28 @@ import Count from "./components/Count";
 import AddOrUpdateTask from "./components/AddOrUpdateTask";
 import TaskList from "./components/TaskList";
 import { Card } from "./components/Card/Card";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="d-flex flex-column">
-      <div className="d-flex">
-        <Card title="Count">
-          <Count />
-        </Card>
-        <Card title="Add Task">
-          <AddOrUpdateTask />
-        </Card>
+    <>
+      <Header />
+      <div className="d-flex flex-column">
+        <div className="d-flex">
+          <Card title="Count">
+            <Count />
+          </Card>
+          <Card title="Add Task">
+            <AddOrUpdateTask />
+          </Card>
+        </div>
+        <div>
+          <Card title="Task List">
+            <TaskList />
+          </Card>
+        </div>
       </div>
-      <div>
-        <Card title="Task List">
-          <TaskList />
-        </Card>
-      </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+    <div className="lato-black d-flex flex-column justify-content-center">
+      <div className="logo">Task</div>
+      <div className="logo">Manager</div>
+    </div>
+    </div>
+  );
+}
